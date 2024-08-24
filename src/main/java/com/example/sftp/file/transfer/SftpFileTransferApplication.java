388fd8a0d8038a276b15ea015cec9b2a722e62dd
nuelladev <emmanuellaokafor42@yahoo.com>
2024-08-24 14:14:08 +0100

    initial commit with dependencies

---
+++ src/main/java/com/example/sftp/file/transfer/SftpFileTransferApplication.java
@@ -0,0 +1,13 @@
+package com.example.sftp.file.transfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SftpFileTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SftpFileTransferApplication.class, args);
+	}
+
+}
